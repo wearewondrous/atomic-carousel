df41c84309743ac5371d925090f8db79dc502de9
Anton Staroverov <tonystarring@gmail.com>
2018-03-23 17:26:00 +0100

    Support options + init in context

--- atomic-slider.js
+++ atomic-slider.js
@@ -1,1 +1,8 @@
 //=include owl.carousel/dist/owl.carousel.min.js
+
+function initSlider(context) {
+  $(context).find("[data-slider]").each(function() {
+    var $this = $(this);
+    $this.addClass('owl-carousel').owlCarousel($this.data('slider'));
+  });
+}
