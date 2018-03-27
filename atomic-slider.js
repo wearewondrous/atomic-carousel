0f76f2d5b894fcdd3f7b4f6c82c87014c89c26c5
Anton Staroverov <tonystarring@gmail.com>
2018-03-27 14:05:34 +0200

    Modifiers, default options and default theme

--- atomic-slider.js
+++ atomic-slider.js
@@ -2,7 +2,13 @@
 
 function initSlider($, context) {
   $(context).find("[data-slider]").each(function() {
-    var $this = $(this);
-    $this.addClass('owl-carousel').owlCarousel($this.data('slider'));
+    var $this = $(this),
+        defaults = {
+          items: 1,
+          nav: true,
+          navText: ['‹', '›']
+        },
+        options = Object.assign({}, defaults, $this.data('slider'));
+    $this.addClass('owl-carousel owl-theme').owlCarousel(options);
   });
 }
