52a3a6a46fecbc9cbbf9403e272a1aa3d1ec82a1
Tassilo Gröper <tassilogroeper@wearewondrous.com>
2020-03-26 10:15:20 +0100

    fix: merged options when multiple carousels are on one page

--- atomic-carousel.js
+++ atomic-carousel.js
@@ -1,25 +1,28 @@
 //=include owl.carousel/dist/owl.carousel.min.js
 
 function initCarousel($, context) {
-  var defaults = {
-    items: 1,
-    nav: true,
-    navText: ['‹', '›'],
-    navElement: 'span' // @TODO: Switch to 'button' once better supported.
-  };
+  var $carousels = $(context).find('[data-carousel]');
 
-  $(context).find('[data-carousel]').each(function() {
-    var $this = $(this);
-    var options = $.extend(defaults, $this.data('carousel'));
+  if (!$carousels.length) {
+    return;
+  }
 
-    $this.owlCarousel(options);
-  });
+  $carousels.each(function() {
+    var defaults = {
+      items: 1,
+      nav: true,
+      navText: ['‹', '›'],
+      navElement: 'span' // @TODO: Switch to 'button' once better supported.
+    };
+    var $owl = $(this);
+    var options = $.extend(defaults, $owl.data('carousel'));
 
-  $(document).on('click', '.owl-item', function() {
-    var $this = $(this);
-    var $owl = $this.closest('.owl-carousel');
-    var owl = $owl.data('owl.carousel');
+    $owl.owlCarousel(options);
 
-    $owl.trigger('to.owl.carousel', owl.relative($this.index()));
+    $owl.on('click', '.owl-item', function() {
+      var owl = $owl.data('owl.carousel');
+
+      $owl.trigger('to.owl.carousel', owl.relative($(this).index()));
+    });
   });
 }
